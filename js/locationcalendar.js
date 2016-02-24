8d475491a3700b9b3cf05ff353cd0d9172fb53eb
Fabian Okeke <fnokeke@gmail.com>
2016-02-24 18:52:19 -0500

    fixed bug so file upload of data is now independent of previous responses before getting to upload section

--- js/locationcalendar.js
+++ js/locationcalendar.js
@@ -75,7 +75,6 @@ var
 
   ui = {
 
-    // ui values are set using user input
     host: window.location.hostname === 'localhost' ?
       'http://localhost:63342/js-viz/' :
       'https://eaf.smalldata.io/partner/slm/',
@@ -100,7 +99,7 @@ var
       var counter = labelName === 'work' ? ui.workCounter++ : ui.hobbyCounter++;
       var inputName = labelName + 'Address' + counter;
       var removeButtonName = 'remove' + inputName;
-      var inputDiv =  inputName.replace(/\d+/g, '') + '-div';
+      var inputDiv = inputName.replace(/\d+/g, '') + '-div';
       inputValue = inputValue || '';
 
       $('<input/>').attr(
@@ -135,13 +134,13 @@ var
 
     // find out if user's location data is from mobility or Google Takeout
     processSourceResponse: function () {
-      var locSource = $('input:radio[name=source]:checked').val();
+      var locSource = $('input:radio[name=locationHistory]:checked').val();
       if (locSource === 'yes') {
         ui.goToAnchor('upload');
-        ui.getUploadedData(function (data) {
-          console.log("data got:", data.length);
-          ui.processGoogleLocation(data);
-        });
+        //ui.getUploadedData(function(data) {
+        //  console.log("data got:", data.length);
+        //  ui.processGoogleLocation(data);
+        //});
       } else if (locSource === 'no') {
         ui.goToAnchor('download');
       }
@@ -178,133 +177,133 @@ var
 
     getUploadedData: function (callback) {
 
-      $('#file').on('change', function () {
-
-        if (!this.files[0]) return;
-
-        utility.modifyDiv('uploadingData-div', 'show');
-        utility.modifyDiv('working-div', 'show');
-
-        var file = this.files[0];
-        var fileSize = prettySize(file.size);
-        var reader = new FileReader();
-
-        function status(message, color) {
-          $('#uploadStatus').text(message);
-          if (color)
-            $('#uploadStatus').css('color', color);
-        }
-
-        function getLocationDataFromJson(data) {
-          utility.assert(data !== '', 'parse json test.');
-          var locations = JSON.parse(data).locations;
-
-          if (!locations || locations.length === 0) {
-            throw new ReferenceError('No location data found.');
-          }
-          return locations;
-        }
-
-        reader.onprogress = function (e) {
-          var percentLoaded = Math.round(( e.loaded / e.total ) * 100);
-          status(percentLoaded + '% of ' + fileSize + ' loaded.', 'grey');
-        };
-
-        reader.onload = function (e) {
-          try {
-            if (e.target.result === '') throw new Error("file too large for this browser. Use Safari.");
-
-            var data = getLocationDataFromJson(e.target.result);
-            status('File loaded successfully! (' + fileSize + ')', 'white');
-            callback(data);
-          } catch (ex) {
-            utility.modifyDiv('working-div', 'hide');
-            status('(Make sure you upload location history file that ends in ".json" Error: ' + ex.message + ')', 'red');
-            callback(-1);
-            return;
-          }
-        };
-
-        reader.onloadend = function (e) {
-          console.log("onloadend: ", _.size(e.target.result));
-          utility.modifyDiv('uploadingData-div', 'hide');
-          utility.modifyDiv('calendar-div', 'show');
-        }
-
-        reader.onerror = function () {
-          utility.modifyDiv('working-div', 'hide');
-          status('Something went wrong reading your JSON file. ' +
-            'Ensure you\'re uploading a "direct-from-Google" JSON file and try again. ' +
-            '(error: ' + reader.error + ')', 'red');
-          callback(-1);
-        };
-
-        reader.readAsText(file);
-
-        //function readBlob(file, callback) {
-        //  //var bigdata = '';
-        //  var bigdataArray = [];
-        //
-        //  var CHUNK_SIZE = 150 * 1048576;
-        //  var start = 0;
-        //  var stop = CHUNK_SIZE;
-        //
-        //  var remainder = file.size % CHUNK_SIZE;
-        //  var blkcount = Math.floor(file.size / CHUNK_SIZE);
-        //  if (remainder != 0) blkcount = blkcount + 1;
-        //
-        //  for (var i = 0; i < blkcount; i++) {
-        //
-        //    var reader = new FileReader();
-        //    if (i == (blkcount - 1) && remainder != 0) {
-        //      stop = start + remainder;
-        //    }
-        //    if (i == blkcount) {
-        //      stop = start;
-        //    }
-        //
-        //    reader.onload = function (e) {
-        //
-        //      try {
-        //        console.log("e.target.result type:", typeof e.target.result);
-        //        bigdataArray.push(e.target.result);
-        //
-        //        var len = 0;
-        //        bigdataArray.forEach(function(str){
-        //          len += str.length;
-        //        });
-        //
-        //        if (len === file.size) {
-        //          console.log("yayyy, big data completed:", len);
-        //          callback(bigdataArray);
-        //        } else {
-        //          console.log("uh oh big data size:", len);
-        //        }
-        //      } catch (ex) {
-        //        console.log("random error:", ex.message);
-        //      }
-        //    }
-        //
-        //    //Slicing the file
-        //    var blob = file.slice(start, stop);
-        //    reader.readAsText(blob);
-        //    start = stop;
-        //    stop = stop + CHUNK_SIZE;
-        //
-        //  } //End of loop
-        //
-        //} //End of readblob
-
-        //readBlob(file, function (value) {
-        //  //getLocationDataFromJson(value);
-        //  //status('File loaded successfully! (' + fileSize + ')', 'green');
-        //  var result = value.join("");
-        //  result = JSON.parse(result);
-        //  console.log("result length:", typeof result.locations.length);
-        //});
-
-
-      });
+      //$('#file').on('change', function () {
+      //
+      //  if (!this.files[0]) return;
+      //
+      //  utility.modifyDiv('uploadingData-div', 'show');
+      //  utility.modifyDiv('working-div', 'show');
+      //
+      //  var file = this.files[0];
+      //  var fileSize = prettySize(file.size);
+      //  var reader = new FileReader();
+      //
+      //  function status(message, color) {
+      //    $('#uploadStatus').text(message);
+      //    if (color)
+      //      $('#uploadStatus').css('color', color);
+      //  }
+      //
+      //  function getLocationDataFromJson(data) {
+      //    utility.assert(data !== '', 'parse json test.');
+      //    var locations = JSON.parse(data).locations;
+      //
+      //    if (!locations || locations.length === 0) {
+      //      throw new ReferenceError('No location data found.');
+      //    }
+      //    return locations;
+      //  }
+      //
+      //  reader.onprogress = function (e) {
+      //    var percentLoaded = Math.round(( e.loaded / e.total ) * 100);
+      //    status(percentLoaded + '% of ' + fileSize + ' loaded.', 'grey');
+      //  };
+      //
+      //  reader.onload = function (e) {
+      //    try {
+      //      if (e.target.result === '') throw new Error("file too large for this browser. Use Safari.");
+      //
+      //      var data = getLocationDataFromJson(e.target.result);
+      //      status('File loaded successfully! (' + fileSize + ')', 'white');
+      //      callback(data);
+      //    } catch (ex) {
+      //      utility.modifyDiv('working-div', 'hide');
+      //      status('(Make sure you upload location history file that ends in ".json" Error: ' + ex.message + ')', 'red');
+      //      callback(-1);
+      //      return;
+      //    }
+      //  };
+      //
+      //  reader.onloadend = function (e) {
+      //    console.log("onloadend: ", _.size(e.target.result));
+      //    utility.modifyDiv('uploadingData-div', 'hide');
+      //    utility.modifyDiv('calendar-div', 'show');
+      //  }
+      //
+      //  reader.onerror = function () {
+      //    utility.modifyDiv('working-div', 'hide');
+      //    status('Something went wrong reading your JSON file. ' +
+      //      'Ensure you\'re uploading a "direct-from-Google" JSON file and try again. ' +
+      //      '(error: ' + reader.error + ')', 'red');
+      //    callback(-1);
+      //  };
+      //
+      //  reader.readAsText(file);
+      //
+      //  //function readBlob(file, callback) {
+      //  //  //var bigdata = '';
+      //  //  var bigdataArray = [];
+      //  //
+      //  //  var CHUNK_SIZE = 150 * 1048576;
+      //  //  var start = 0;
+      //  //  var stop = CHUNK_SIZE;
+      //  //
+      //  //  var remainder = file.size % CHUNK_SIZE;
+      //  //  var blkcount = Math.floor(file.size / CHUNK_SIZE);
+      //  //  if (remainder != 0) blkcount = blkcount + 1;
+      //  //
+      //  //  for (var i = 0; i < blkcount; i++) {
+      //  //
+      //  //    var reader = new FileReader();
+      //  //    if (i == (blkcount - 1) && remainder != 0) {
+      //  //      stop = start + remainder;
+      //  //    }
+      //  //    if (i == blkcount) {
+      //  //      stop = start;
+      //  //    }
+      //  //
+      //  //    reader.onload = function (e) {
+      //  //
+      //  //      try {
+      //  //        console.log("e.target.result type:", typeof e.target.result);
+      //  //        bigdataArray.push(e.target.result);
+      //  //
+      //  //        var len = 0;
+      //  //        bigdataArray.forEach(function(str){
+      //  //          len += str.length;
+      //  //        });
+      //  //
+      //  //        if (len === file.size) {
+      //  //          console.log("yayyy, big data completed:", len);
+      //  //          callback(bigdataArray);
+      //  //        } else {
+      //  //          console.log("uh oh big data size:", len);
+      //  //        }
+      //  //      } catch (ex) {
+      //  //        console.log("random error:", ex.message);
+      //  //      }
+      //  //    }
+      //  //
+      //  //    //Slicing the file
+      //  //    var blob = file.slice(start, stop);
+      //  //    reader.readAsText(blob);
+      //  //    start = stop;
+      //  //    stop = stop + CHUNK_SIZE;
+      //  //
+      //  //  } //End of loop
+      //  //
+      //  //} //End of readblob
+      //
+      //  //readBlob(file, function (value) {
+      //  //  //getLocationDataFromJson(value);
+      //  //  //status('File loaded successfully! (' + fileSize + ')', 'green');
+      //  //  var result = value.join("");
+      //  //  result = JSON.parse(result);
+      //  //  console.log("result length:", typeof result.locations.length);
+      //  //});
+      //
+      //
+      //});
     },
 
     useInputProvided: function () {
@@ -359,7 +358,7 @@ var
       }
 
       if (!(hasValidHome && hasValidWork && hasValidHobby && daysCount !== '')) {
-        window.location.href = ui.host + '#address';
+        ui.goToAnchor('address');
         $('#addressStatus').text('Please complete all fields.');
         $('#addressStatus').css('color', 'red');
         return;
@@ -408,6 +407,7 @@ var
     },
 
     processGoogleLocation: function (uploadedData) {
+      console.log("so processGoogleLocation has been called.");
 
       // convert full addresses to lat,lon
       var getLatLng = function (allMappedAddresses, callback) {
@@ -855,7 +855,7 @@ var
                 utility.modifyDiv('calendar-div', 'hide');
                 utility.modifyDiv('working-div', 'hide');
 
-                window.location.href = ui.host + '#calendarView';
+                ui.goToAnchor('calendarView');
               });
             }
 
@@ -946,3 +946,132 @@ var
 //TODO: let users know that they have to be patient because the archive download could actually be slow
 //TODO: people don't know what to view or expect when the calendar finally pops up
 
+
+(function () {
+
+  $('#file').on('change', function () {
+    console.log("file state changed!");
+
+    if (!this.files[0]) return;
+
+    utility.modifyDiv('uploadingData-div', 'show');
+    utility.modifyDiv('working-div', 'show');
+
+    var file = this.files[0];
+    var fileSize = prettySize(file.size);
+    var reader = new FileReader();
+
+    function status(message, color) {
+      $('#uploadStatus').text(message);
+      if (color)
+        $('#uploadStatus').css('color', color);
+    }
+
+    function getLocationDataFromJson(data) {
+      utility.assert(data !== '', 'parse json test.');
+      var locations = JSON.parse(data).locations;
+
+      if (!locations || locations.length === 0) {
+        throw new ReferenceError('No location data found.');
+      }
+      return locations;
+    }
+
+    reader.onprogress = function (e) {
+      var percentLoaded = Math.round(( e.loaded / e.total ) * 100);
+      status(percentLoaded + '% of ' + fileSize + ' loaded.', 'grey');
+    };
+
+    reader.onload = function (e) {
+      try {
+        if (e.target.result === '') throw new Error("file too large for this browser. Use Safari.");
+
+        var data = getLocationDataFromJson(e.target.result);
+        status('File loaded successfully! (' + fileSize + ')', 'white');
+        ui.processGoogleLocation(data);
+      } catch (ex) {
+        utility.modifyDiv('working-div', 'hide');
+        status('(Make sure you upload location history file that ends in ".json" Error: ' + ex.message + ')', 'red');
+        return;
+      }
+    };
+
+    reader.onloadend = function (e) {
+      console.log("onloadend: ", _.size(e.target.result));
+      utility.modifyDiv('uploadingData-div', 'hide');
+      utility.modifyDiv('calendar-div', 'show');
+    }
+
+    reader.onerror = function () {
+      utility.modifyDiv('working-div', 'hide');
+      status('Something went wrong reading your JSON file. ' +
+        'Ensure you\'re uploading a "direct-from-Google" JSON file and try again. ' +
+        '(error: ' + reader.error + ')', 'red');
+    };
+
+    reader.readAsText(file);
+
+    //function readBlob(file, callback) {
+    //  //var bigdata = '';
+    //  var bigdataArray = [];
+    //
+    //  var CHUNK_SIZE = 150 * 1048576;
+    //  var start = 0;
+    //  var stop = CHUNK_SIZE;
+    //
+    //  var remainder = file.size % CHUNK_SIZE;
+    //  var blkcount = Math.floor(file.size / CHUNK_SIZE);
+    //  if (remainder != 0) blkcount = blkcount + 1;
+    //
+    //  for (var i = 0; i < blkcount; i++) {
+    //
+    //    var reader = new FileReader();
+    //    if (i == (blkcount - 1) && remainder != 0) {
+    //      stop = start + remainder;
+    //    }
+    //    if (i == blkcount) {
+    //      stop = start;
+    //    }
+    //
+    //    reader.onload = function (e) {
+    //
+    //      try {
+    //        console.log("e.target.result type:", typeof e.target.result);
+    //        bigdataArray.push(e.target.result);
+    //
+    //        var len = 0;
+    //        bigdataArray.forEach(function(str){
+    //          len += str.length;
+    //        });
+    //
+    //        if (len === file.size) {
+    //          console.log("yayyy, big data completed:", len);
+    //          callback(bigdataArray);
+    //        } else {
+    //          console.log("uh oh big data size:", len);
+    //        }
+    //      } catch (ex) {
+    //        console.log("random error:", ex.message);
+    //      }
+    //    }
+    //
+    //    //Slicing the file
+    //    var blob = file.slice(start, stop);
+    //    reader.readAsText(blob);
+    //    start = stop;
+    //    stop = stop + CHUNK_SIZE;
+    //
+    //  } //End of loop
+    //
+    //} //End of readblob
+
+    //readBlob(file, function (value) {
+    //  //getLocationDataFromJson(value);
+    //  //status('File loaded successfully! (' + fileSize + ')', 'green');
+    //  var result = value.join("");
+    //  result = JSON.parse(result);
+    //  console.log("result length:", typeof result.locations.length);
+    //});
+
+  });
+}());
